9750b3f7c98b0b54420bf5c81c4e0e6892377d6f
Looseminded <31506967+Looseminded@users.noreply.github.com>
2022-03-13 19:25:17 -0700

    Add files via upload

---
+++ randomsurvivor.py
@@ -0,0 +1,8 @@
+import random
+import time
+
+survivors = ['Commando', 'Huntress', 'Bandit', 'Mul-T', 'Engineer', 'Artificer', 'Mercenary', 'REX', 'Loader', 'Acrid', 'Captain', 'Railgunner', 'Void Fiend']
+
+print(random.choice(survivors))
+
+time.sleep(5)
